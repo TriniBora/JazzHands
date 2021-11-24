fc95902a463d0b0bf39d523a8ae0fe29cc8bb5e0
Trinidad Boragini <54488386+TriniBora@users.noreply.github.com>
2021-11-24 13:30:30 -0300

    Delete usuarios.sql

--- usuarios.sql
+++
@@ -1,3 +0,0 @@
-CREATE TABLE `jazz`.`usuarios` ( `id_usuario` INT(255) NOT NULL AUTO_INCREMENT , `username` VARCHAR(50) NOT NULL , `password` VARCHAR(10) NOT NULL , PRIMARY KEY (`id_usuario`)) ENGINE = InnoDB;
-
-INSERT INTO `jazz`.`usuarios` (`id_usuario`, `username`, `password`) VALUES (NULL, 'admin', 'admin');
